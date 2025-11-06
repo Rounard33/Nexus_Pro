69aa1a0a3f72e12c7aace5bf72a1a56ba8fe4d61
mehrsam <mathieu.ehrsam@atinternet.com>
2025-11-06 13:50:12 +0100

    amélioration design

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -96,8 +96,6 @@ export class ContactComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   contactInfo = {
-    email: 'contact@reikietsens.com',
-    phone: '+33 6 74 70 09 66',
     address: '132 route de Capian - 33550 Langoiran'
   };
 
